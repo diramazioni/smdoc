3c82ce00a9ab7a02db0dcd500329e118778cc74c
Eli Spizzichino <eli@diramazioni.net>
2024-12-20 11:17:37 +0100

    fix edit on click the edit  icon

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -12,14 +12,13 @@
   <!-- svelte-ignore a11y_click_events_have_key_events -->
   <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
   {#if $page.data.user}
-    <div id="content" class="m-4 cursor-pointer" 
-    onclick={() => goto('/edit' + $page.url.pathname)}
+    <div id="content" class="m-4" 
     onmouseenter={() => showEditDiv = true} onmouseleave={() => showEditDiv = false} role="region">
       {#if showEditDiv}
-        <div class="flex justify-center items-center">
-          <div id="edit" class="absolute">
-            <a href="/edit{$page.url.pathname}">
-              <Pencil />
+        <div class="cursor-pointer flex justify-center items-center">
+          <div id="edit" class="fixed top-20 hover:bg-slate-200 p-4 rounded-lg border " >
+            <a href="/edit{$page.url.pathname}" onclick={() => goto('/edit' + $page.url.pathname)}>
+              <Pencil size="40"/>
             </a>
           </div>
         </div>
