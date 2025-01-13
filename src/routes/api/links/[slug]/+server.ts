66956d6db8090fbae8a83580c1bfb4318209a537
Eli Spizzichino <eli@diramazioni.net>
2025-01-14 00:19:26 +0100

    [routes] fix api/links route

---
+++ src/routes/api/links/[slug]/+server.ts
@@ -0,0 +1,28 @@
+import { error } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { writeFile, readFile } from 'node:fs/promises';
+import { join } from 'path';
+export const GET: RequestHandler = async ({ url, params }) => {
+	try {
+        const slug = params.slug;
+		// Path for Markdown file containg the navigation list
+		const navigationPath = join(process.cwd(), 'mdocs', `${slug}.md`);
+		const navListData = await readFile(navigationPath, 'utf8');
+        
+        // Parse the Markdown file to extract links and return as a JSON response
+        const linksArray = navListData.split('\n')
+            .map(line => {
+                const match = line.match(/\[([^\]]+)\]\(([^)]+)\)/);
+                return match ? { href: match[2].replace('.md',''), title: match[1] } : null;
+            })
+            .filter(item => item !== null);
+
+        return new Response(JSON.stringify(linksArray), {
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+	} catch (err) {
+		console.error('Error reading navigation file:', err);
+		throw error(500, 'Unable to read the Markdown file');
+	}
+};
--- src/routes/home/+page.server.ts
+++ src/routes/home/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from './$types'
 // get `locals.user` and pass it to the `page` store
 export const load: PageServerLoad = async ({ fetch }) => {
   return {
-    links: await fetch('/links/home')
+    links: await fetch('/api/links/home')
     .then((res) => res.json())
     .then((data) => data)
     .catch((err) => console.log(err))
